4be6a4d2409fc5179f2cc68367a2f87a727c135c
kaylagarcia614 <130188636+kaylagarcia614@users.noreply.github.com>
2023-10-11 13:21:07 -0700

    step 2

--- src/main.ts
+++ src/main.ts
@@ -2,17 +2,44 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
+let elements_to_add: HTMLElement[] = [];
+elements_to_add = [];
+
 const gameName = "My very cool game";
-const buttonName = "🎉";
+const buttonName = "🐟";
 
 document.title = gameName;
 
+//header
 const header = document.createElement("header1");
-
 header.innerHTML = gameName;
+header.style.position = "";
+elements_to_add.push(header);
 
+//counter
+let counter: number = 0;
+const counter_element = document.createElement("div");
+counter_element.style.fontSize = "35px";
+update_counter(counter_element, counter);
+elements_to_add.push(counter_element);
+
+//button
 const button = document.createElement("button");
 button.textContent = buttonName;
+button.style.fontSize = "40px";
+elements_to_add.push(button);
 
-app.append(header);
-app.append(button);
+// click
+const num_to_increase: number = 1;
+button.addEventListener("click", () => {
+  counter = counter + num_to_increase;
+  update_counter(counter_element, counter);
+});
+
+function update_counter(count_element: HTMLDivElement, counter: number) {
+  count_element.innerHTML = counter + " fishies captured";
+}
+
+elements_to_add.forEach((elem) => {
+  app.append(elem);
+});
