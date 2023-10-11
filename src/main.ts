4aac013f9634af0fc427dc9b3c283bf4a1b2cae2
kaylagarcia614 <130188636+kaylagarcia614@users.noreply.github.com>
2023-10-11 13:01:29 -0700

    step 1

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,16 @@ import "./style.css";
 const app: HTMLDivElement = document.querySelector("#app")!;
 
 const gameName = "My very cool game";
+const buttonName = "🎉";
 
 document.title = gameName;
 
-const header = document.createElement("h1");
+const header = document.createElement("header1");
 
 header.innerHTML = gameName;
+
+const button = document.createElement("button");
+button.textContent = buttonName;
+
 app.append(header);
+app.append(button);
