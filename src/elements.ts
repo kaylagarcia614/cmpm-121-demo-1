ad5b3d0899886989803c4be29277943e4317fd20
kaylagarcia614 <130188636+kaylagarcia614@users.noreply.github.com>
2023-10-11 16:11:41 -0700

    step 5

---
+++ src/elements.ts
@@ -0,0 +1,99 @@
+// Define a class to represent the state of the world
+export class world_state {
+  per_click_increase: number; // Amount of fishies added per click
+  fishies_per_second: number; // Fishies generated per second
+  total_fishies: number; // Total count of fishies
+  elements_to_add: HTMLElement[] = []; // Array to hold HTML elements
+  total_fishies_count_element: HTMLDivElement; // Element to display total fishies count
+  upgrade_buttons: upgrade[] = []; // Array to hold upgrade buttons
+  fishies_per_second_count_element: HTMLDivElement | null = null; // Element to display fishies per second
+
+  constructor(total_fishies_counter_element: HTMLDivElement) {
+    this.elements_to_add = [];
+    this.upgrade_buttons = [];
+
+    this.per_click_increase = 1; // Default click increase value
+    this.fishies_per_second = 0; // Default fishies per second value
+    this.total_fishies = 0; // Default total fishies count
+    this.total_fishies_count_element = total_fishies_counter_element; // Element to display total fishies count
+  }
+
+  // Method to update the total fishies count and update the displayed text
+  update_counter(count: number, text: string = " fishies captured") {
+    this.total_fishies = count + this.total_fishies;
+    this.total_fishies_count_element.innerHTML = this.total_fishies + text;
+    this.check_buttons();
+  }
+
+  // Method to check and enable/disable upgrade buttons based on total fishies count
+  check_buttons() {
+    this.upgrade_buttons.forEach((btn) => {
+      if (this.total_fishies >= btn.cost) {
+        btn.button_element.disabled = false;
+      } else {
+        btn.button_element.disabled = true;
+      }
+    });
+  }
+}
+
+// Define a class to represent an upgrade button
+export class upgrade {
+  button_element: HTMLButtonElement; // The HTML button element
+  cost: number; // Cost of the upgrade
+  per_second_increase: number; // Increase in fishies per second
+  price_scaling: number; // Scaling factor for the upgrade cost
+  emoji_name: string; // Name or emoji associated with the upgrade
+  world_state: world_state; // Reference to the world state
+
+  constructor(
+    emoji: string,
+    cost: number,
+    psi: number,
+    p_scaling: number,
+    world: world_state,
+  ) {
+    this.emoji_name = emoji;
+    this.button_element = document.createElement("button");
+    this.button_element.style.fontSize = "40pt";
+    this.cost = cost;
+    this.per_second_increase = psi;
+    this.price_scaling = p_scaling;
+    this.world_state = world;
+
+    // Set the text and disable the button by default
+    this.button_element.innerHTML =
+      this.emoji_name +
+      " Cost: " +
+      cost +
+      " | " +
+      this.per_second_increase +
+      " fps";
+    this.button_element.disabled = true;
+
+    // Add the button element to the world's elements_to_add array
+    world.elements_to_add.push(this.button_element);
+
+    // Add a click event listener to the button
+    this.button_element.addEventListener("click", () => {
+      world.update_counter(cost * -1, undefined); // Update total fishies count
+      this.cost = this.cost * this.price_scaling; // Increase the cost for the next upgrade
+      this.update_fishies_per_second(); // Update fishies per second count
+    });
+
+    // Add the upgrade button to the world's upgrade_buttons array
+    this.world_state.upgrade_buttons.push(this);
+  }
+
+  // Method to update the fishies per second count
+  update_fishies_per_second() {
+    this.world_state.fishies_per_second =
+      this.world_state.fishies_per_second + this.per_second_increase;
+
+    // If there's a fishies per second count element, update its text
+    if (this.world_state.fishies_per_second_count_element != null) {
+      this.world_state.fishies_per_second_count_element.innerHTML =
+        this.world_state.fishies_per_second + " fps";
+    }
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,88 +1,81 @@
-// Import the CSS file to apply styles to the page
 import "./style.css";
+import { world_state, upgrade } from "./elements"; // Import the world_state and upgrade classes from the 'elements' module
 
-// Define constants and variables for the game
-const per_click_increase: number = 1; // Increase in fishies per click
-const fishies_per_second: number = 1; // Fishies per second for auto click
-let total_fishies: number = 0; // Total fishies collected
+const app: HTMLDivElement = document.querySelector("#app")!; // Select the HTML element with id "app"
 
-// Get the app container from the HTML document
-const app: HTMLDivElement = document.querySelector("#app")!;
+// Create a counter element to display the total fishies captured
+const total_fishies_counter_element = document.createElement("div");
+total_fishies_counter_element.style.fontSize = "40pt";
+total_fishies_counter_element.innerHTML = 0 + " fishies captured";
 
-// Initialize an array to store HTML elements
-const elements_to_add: HTMLElement[] = [];
+// Create a player instance with the world_state and attach the total fishies counter
+const player: world_state = new world_state(total_fishies_counter_element);
+player.elements_to_add.push(total_fishies_counter_element); // Add the counter element to the list of elements to add
 
-// Set the game name and title
+// Set game title and create header element
 const gameName = "fishy fish";
 const buttonName = "🐟";
 document.title = gameName;
+create_new_element("header", "", gameName, true); // Create a header element and add it to the player's elements_to_add
 
-// Create a header element with the game name
-create_new_element("header1", "", gameName);
+// Create a button element
+const button = create_new_element("button", "50px", buttonName);
 
-// Create a counter element to display the total fishies captured
-const total_fishies_counter_element = create_new_element(
-  "div",
-  "35px",
-  total_fishies + " fishies captured",
-);
-
-// Create a button element with the fish emoji
-const button = create_new_element("button", "70px", buttonName);
-
-// Add a click event listener to the button to increase the fishies count
+// Add a click event listener to the button to increase fishies count when clicked
 button.addEventListener("click", () => {
-  update_counter(
-    total_fishies_counter_element,
-    per_click_increase,
-    " fishies captured",
-  );
+  player.update_counter(player.per_click_increase);
 });
 
-// Create an element to display auto clicks per second
-create_new_element("div", "35px", fishies_per_second + " fps");
+// Create an element to display the fishies per second count
+const autoclick_element = create_new_element(
+  "div",
+  "30px",
+  player.fishies_per_second + " fps",
+);
+player.fishies_per_second_count_element = autoclick_element as HTMLDivElement; // Set the fishies per second count element in the world_state
+start_auto_counter(player); // Start automatic fishies per second counter
 
-// Start auto counter for fishies per second
-start_auto_counter(total_fishies_counter_element, fishies_per_second);
+// Create an upgrade button
+new upgrade("⛵️", 10, 1, 1, player); // Create an upgrade button with the specified parameters
 
-// Add all created elements to the app container
-elements_to_add.forEach((elem) => {
+// Add all elements to the app in the order they were created
+player.elements_to_add.forEach((elem) => {
   app.append(elem);
 });
 
-// Function to create a new HTML element
-function create_new_element(type: string, font_size: string, text: string) {
+// Function to create a new HTML element and add it to the player's elements_to_add
+function create_new_element(
+  type: string,
+  font_size: string,
+  text: string,
+  unshift: boolean = false,
+): HTMLElement | HTMLButtonElement | HTMLDivElement {
   const new_element = document.createElement(type);
   new_element.style.fontSize = font_size;
   new_element.innerHTML = text;
-  elements_to_add.push(new_element); // Add the element to the array
+
+  if (unshift) {
+    player.elements_to_add.unshift(new_element); // Add to the beginning of the list
+  } else {
+    player.elements_to_add.push(new_element); // Add to the end of the list
+  }
 
   return new_element;
 }
 
-// Function to update the fishies counter
-function update_counter(
-  count_element: HTMLDivElement | HTMLElement,
-  count: number,
-  text: string = " fishies captured",
-) {
-  total_fishies = total_fishies + count;
-  count_element.innerHTML = total_fishies + text;
-}
-
-// Function to automatically update the counter at a specified rate
-function start_auto_counter(elem: HTMLElement, pps: number) {
+// Function to automatically update the counter based on fishies per second
+function start_auto_counter(player_world_state: world_state) {
   let past_date = performance.now();
   window.requestAnimationFrame(check_for_counter_update);
 
   function check_for_counter_update() {
-    const milliseconds_per_update = 1000 / pps;
+    const milliseconds_per_update =
+      1000 / player_world_state.fishies_per_second;
 
-    // Check if it's time to update the counter and update it
     if (performance.now() - past_date > milliseconds_per_update) {
-      update_counter(elem, 1);
+      player.update_counter(1); // Update the counter by 1 fishie
       past_date = performance.now();
     }
-    window.requestAnimationFrame(check_for_counter_update); // Request the next frame
+    window.requestAnimationFrame(check_for_counter_update); // Continue checking for updates
   }
 }
